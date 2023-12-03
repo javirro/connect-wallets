f0a0205cb24a063e1203ae8dcba7762958f8bd42
idcetim <javirofe96@gmail.com>
2023-12-03 10:36:47 +0100

    Set connectWalletModalConfiguration

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,35 @@ import React from "react";
 
 import "./App.css";
 import {
-  createWeb3Modal,
-  defaultConfig,
-  useWeb3ModalAccount,
-} from "@web3modal/ethers5/react";
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygon, polygonMumbai } from "viem/chains";
 import ConnectButton from "./ConnectWallet/ConnectionButton";
 
+const chains = [
+  process.env.REACT_APP_IS_PRODUCTION === "true" ? polygon : polygonMumbai,
+];
+const projectId = process.env.REACT_APP_PROJECT_ID as string;
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
 function App() {
-  const { address, chainId, isConnected } = useWeb3ModalAccount();
-  const PROJECT_ID: string = process.env.REACT_APP_PROJECT_ID as string;
-
-  const mainnet = {
-    chainId: 1,
-    name: "Ethereum",
-    currency: "ETH",
-    explorerUrl: "https://etherscan.io",
-    rpcUrl: "https://cloudflare-eth.com",
-  };
-
-  const metadata = {
-    name: "My Website",
-    description: "My Website description",
-    url: "https://mywebsite.com",
-    icons: ["https://avatars.mywebsite.com/"],
-  };
-
-  createWeb3Modal({
-    ethersConfig: defaultConfig({ metadata }),
-    chains: [mainnet],
-    projectId: PROJECT_ID,
-    featuredWalletIds: [
-      "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393",
-      "19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927",
-      "ecc4036f814562b41a5268adc86270fba1365471402006302e70169465b7ac18",
-    ],
-    themeMode: "dark",
-    themeVariables: {
-      "--w3m-color-mix": "#000000",
-      "--w3m-color-mix-strength": 40,
-      "--w3m-accent": "#FFFFFF",
-    },
-  });
-
   return (
     <div className="App">
-      <ConnectButton />
-
-      <h2>{address}</h2>
+      <WagmiConfig config={wagmiConfig}>
+        <ConnectButton />
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </div>
   );
 }
--- src/ConnectWallet/ConnectionButton.tsx
+++ src/ConnectWallet/ConnectionButton.tsx
@@ -1,17 +1,26 @@
-import { useWeb3Modal } from "@web3modal/ethers5/react";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
+import { useWeb3Modal } from "@web3modal/react";
 
-export type OpenModalTypes = "Networks" | "Connect";
 const ConnectButton = () => {
-  // 4. Use modal hook
-
   const { open } = useWeb3Modal();
+  const { isConnected: isPolygonConnected, address: polygonAddress } =
+    useAccount();
+  const { chain } = useNetwork();
+  const { chains, switchNetwork }: { chains: any[]; switchNetwork: any } =
+    useSwitchNetwork();
+
+  useAccount({
+    onConnect: async () => {
+      const usedChain: number = chains[0].id;
+      if ((chain?.id as number) !== (chains[0].id as number)) {
+        switchNetwork(usedChain);
+      }
+    },
+  });
 
   return (
     <>
       <button onClick={() => open()}>Open Connect Modal</button>
-      <button onClick={() => open({ view: "Networks" })}>
-        Open Network Modal
-      </button>
     </>
   );
 };
